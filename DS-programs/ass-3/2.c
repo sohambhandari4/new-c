bef2b2e1c14e7e3dccd6837e20f77268fbfe4cf2
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2024-04-19 09:21:21 +0530

    Update 2.c

--- DS-programs/ass-3/2.c
+++ DS-programs/ass-3/2.c
@@ -1,8 +1,8 @@
 //2)	Write a C program to accept n elements from user store it in an array.
 // Accept a value from the user and use recursive binary search method to check whether the value is present in array or not.
 //  Display proper message.
-#include <stdio.h>
 
+#include <stdio.h>
 int binarySearch(int arr[], int left, int right, int key) 
 {
     if (left <= right)
@@ -23,25 +23,20 @@ int binarySearch(int arr[], int left, int right, int key)
     }
 
     return 0; // Element not found
-}
-
+}
 int main() {
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-    int arr[n];
-
+    int arr[n];
     printf("Enter %d elements (in sorted order):\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-    }
-
+    }
     int key;
     printf("Enter the value to search: ");
-    scanf("%d", &key);
-
-    int result = binarySearch(arr, 0, n - 1, key);
-
+    scanf("%d", &key);
+    int result = binarySearch(arr, 0, n - 1, key);
     if (result) 
     {
         printf("Element %d is present in the array.\n", key);
@@ -49,7 +44,6 @@ int main() {
     else
      {
         printf("Element %d is not present in the array.\n", key);
-    }
-
+    }
     return 0;
 }
